91720c93b16d67f00450e1b1656da5f648f3ebcc
matheusssl <83477899+matheusssl@users.noreply.github.com>
2022-11-17 23:53:35 -0300

    Add files via upload

---
+++ Calculadora/calculadora.py
@@ -0,0 +1,31 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+print("+ para Adição")
+print("- para Subtração")
+print("* para Multiplicação")
+print("/ para Divisão")
+print("** para Potenciação")
+
+operacao = input('Digite a operação desejada: ')
+resultado = 0
+
+if operacao == ('+'):
+    resultado = n1 + n2
+
+elif operacao == ('-'):
+    resultado = n1 - n2
+
+elif operacao == ('*'):
+    resultado = n1 * n2
+
+elif operacao == ('/'):
+    resultado = n1 / n2
+
+elif operacao == ('**'):
+    resultado = n1 ** n2
+
+else:
+    print('Operação não disponível, encerrando calculadora!')
+
+print(n1, operacao, n2, '=', resultado)
